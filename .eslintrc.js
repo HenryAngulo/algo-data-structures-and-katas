13b139c56afcf4884216d3cb1eaa85206df09210
Henry Angulo <hangulo@saltala.com>
2022-01-11 00:24:03 -0300

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,67 @@
+module.exports = {
+  root: true,
+  parser: `babel-eslint`,
+  env: {
+    node: true,
+    es6: true,
+  },
+  parserOptions: { ecmaVersion: 8 }, // to enable features such as async/await
+  ignorePatterns: [`node_modules/*`, `.next/*`, `.out/*`, `!.prettierrc.js`], // We don't want to lint generated files nor node_modules, but we want to lint .prettierrc.js (ignored by default by eslint)
+  extends: [
+    `eslint:recommended`,
+    `plugin:prettier/recommended`, //Prettier plugin,
+  ],
+  rules: {
+    //General rules
+    'object-curly-newline': `off`,
+    'no-mixed-operators': `off`,
+    'no-plusplus': 1,
+    semi: [`error`, `never`],
+    'max-len': [
+      `error`,
+      80,
+      2,
+      {
+        ignoreUrls: true,
+        ignoreRegExpLiterals: true,
+        ignoreTemplateLiterals: true,
+      },
+    ],
+    'no-console': `warn`,
+    radix: 1,
+    'prefer-destructuring': `off`,
+    'prefer-const': [
+      `error`,
+      {
+        destructuring: `all`,
+        ignoreReadBeforeAssign: false,
+      },
+    ],
+    'no-useless-concat': `error`,
+    'no-negated-condition': 0,
+    quotes: [`error`, `backtick`],
+    'no-shadow': 0,
+    'no-multi-spaces': 2,
+    'no-multiple-empty-lines': [
+      `error`,
+      {
+        max: 1,
+        maxEOF: 1,
+      },
+    ],
+    'no-irregular-whitespace': 2,
+    camelcase: [
+      2,
+      {
+        properties: `always`,
+      },
+    ],
+    'no-trailing-spaces': [
+      2,
+      {
+        skipBlankLines: true,
+      },
+    ],
+    'prettier/prettier': [`warn`],
+  },
+}
---
+++ exercises/footbalScores.js
@@ -0,0 +1,64 @@
+/* eslint-disable max-len */
+/*
+The number of goals achieved by two football teams in matches in a league is given in the form of two lists. For each match of team B, compute the total number of matches of team A where team A has scored less than or equal to the number of goals scored by team B in that match.
+
+Example
+
+teamA = [1, 2, 3]
+
+teamB = [2, 4]
+
+Team A has played three matches and has scored teamA = [1, 2, 3] goals in each match respectively. Team B has played two matches and has scored teamB = [2, 4] goals in each match respectively. For 2 goals scored by team B in its first match, team A has 2 matches with scores 1 and 2. For 4 goals scored by team B in its second match, team A has 3 matches with scores 1, 2 and 3. Hence, the answer is [2, 3].
+
+Function Description
+
+Complete the function counts in the editor below.
+
+counts has the following parameter(s):
+
+    int teamA[n]:  first array of positive integers
+
+    int teamB[m]:  second array of positive integers
+
+Return
+
+    int[m]: an array of m positive integers, one for each teamB[i] representing the total number of elements from teamA[j] satisfying teamA[j] ≤ teamB[i] where 0 ≤ j < n and 0 ≤ i < m, in the given order.
+
+Constraints
+
+2 ≤ n, m ≤ 105
+1 ≤ teamA[j] ≤ 109, where 0 ≤ j < n.
+1 ≤ teamB[i] ≤ 109, where 0 ≤ i < m.
+*/
+
+function binarySearchAndReturnNumberOfLowerValues(array, value) {
+  let lowerBound = 0
+  let upperBound = array.length - 1
+  while (lowerBound <= upperBound) {
+    const midPoint = Math.floor((lowerBound + upperBound) / 2)
+    const midValue = array[midPoint]
+    const nextValue = array[midPoint + 1]
+    if (midValue <= value && nextValue > value) {
+      return midPoint + 1
+    }
+    if (midValue > value) {
+      upperBound = midPoint - 1
+    } else {
+      lowerBound = midPoint + 1
+    }
+  }
+  return upperBound + 1
+}
+
+function counts(teamA, teamB) {
+  // Write your code here
+  teamA.sort((a, b) => a - b)
+  const lessOrEqualTeamAMatches = teamB.map((goalsB) =>
+    binarySearchAndReturnNumberOfLowerValues(teamA, goalsB),
+  )
+  return lessOrEqualTeamAMatches
+}
+
+const testCase = counts([2, 10, 5, 4, 8], [3, 1, 7, 8])
+
+return testCase
